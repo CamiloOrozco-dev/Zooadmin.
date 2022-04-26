bc61accbe4dc786122e3be8998beb2c1639b53d5
Shen2u <85773222+Shendev-hub@users.noreply.github.com>
2022-04-26 13:26:04 -0500

    intento conexión con la base de tados

--- controller/registro.php
+++ controller/registro.php
@@ -7,7 +7,11 @@ if (!empty($_POST['DNI']&& !empty($_POST['nombre'] && !empty($_POST['rpassword']
     $sql= "INSERT INTO usuario (DNI, nombre, rpassword) VALUES (:DNI, :nombre, :rpassword)";
     $stmt = $conn->prepare ($sql);
     $stmt->bindParam(':DNI',$_POST['DNI']);
+    $stmt->bindParam(':tipo_DNI',$_POST['tipo_DNI']);
     $stmt->bindParam(':nombre',$_POST['nombre']);
+    $stmt->bindParam(':apellido',$_POST['apellido']);
+    $stmt->bindParam(':telefono',$_POST['telefono']);
+    $stmt->bindParam(':email',$_POST['email']);
     $rpassword = password_hash($_POST['rpassword'], PASSWORD_BCRYPT);
     $stmt->bindParam(':rpassword',$rpassword);
   
