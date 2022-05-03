d1aaa18fa7403ec846169214057c41cb84d4cba3
Shen2u <85773222+Shendev-hub@users.noreply.github.com>
2022-05-02 21:30:39 -0500

    correción

--- controller/login.php
+++ controller/login.php
@@ -1,12 +1,38 @@
 <!-- TODO: terminar el regstro con los datos minuto del video 43:24 -->
+<?php
+require 'conexion.php';
+$message="";
+//TODO: validar campos requeridos
+if ($_POST){
+if (!empty($_POST['DNI']&& !empty($_POST['nombre'] && !empty($_POST['rpassword'])))) {
 
-<!-- TODO: validar campos requeridos -->
+    $sql= "INSERT INTO registro (DNI, nombre, rpassword) VALUES (:DNI, :nombre, :rpassword)";
+    $stmt = $conn->prepare ($sql);
+    $stmt->bindParam(':DNI',$_POST['DNI']);
+    $stmt->bindParam(':tipo_DNI',$_POST['tipo_DNI']);
+    $stmt->bindParam(':nombre',$_POST['nombre']);
+    $stmt->bindParam(':apellido',$_POST['apellido']);
+    $stmt->bindParam(':telefono',$_POST['telefono']);
+    $stmt->bindParam(':email',$_POST['email']);
+    $rpassword = password_hash($_POST['rpassword'], PASSWORD_BCRYPT);
+    $stmt->bindParam(':rpassword',$rpassword);
+  
+    if ($stmt->execute()) {
 
+       $message ='Successfully created new user ';
+    } else {
+        $message ='Sorry there must have been an issue creating your account  ';
+    }
+}
+}
+?>
 <?php 
 require 'conexion.php';
+if ($_POST){
 if (!empty($_POST['DNI'] && !empty ($_POST['rpassword']))) {
 
     $records = $conn->prepare ('SELECT DNI, tipo_DNI, nombre, apellido, telefono, email, rpassword SELECT FROM registro WHERE email=:email');
+}
 }
 ?>
 <!DOCTYPE html>
